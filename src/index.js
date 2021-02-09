eaace033803a8c7d15af62611ca376491b73a5cb
Natalie <natalie.coleman39@gmail.com>
2021-02-09 15:18:53 -0500

    Fix build configuration

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from "react-dom";
-ReactDOM.render(_jsx("div", { children: "Brew Software: REACT REDUX" }, void 0), document.getElementById("root"));
